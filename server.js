f9104384eaf673524107260f60363422c27a60bb
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-08-10 21:11:32 +0200

    scritto decentemente

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var path = require("path");
-var express = require("express");
-
-var app = express();
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.set("port", process.env.PORT || 8080);
-
-var server = app.listen(app.get("port"), function () {
-  console.log("listening on port ", server.address().port);
+app.listen(port, () => {
+  console.info(`Server listen at ${port} ...`);
 });
