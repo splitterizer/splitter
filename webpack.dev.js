22a254573a9ed0eb53eb97809cdc0c66dd633331
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-08-18 21:33:36 +0200

    non usato

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
