0f039b857ec01fe44703391316573dabeb290da4
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-08-19 18:11:10 +0200

    fix test bucato

--- tests/Vacanza.test.ts
+++ tests/Vacanza.test.ts
@@ -5,6 +5,6 @@ describe("Vacanza", () => {
   it("Instanziare la calsse con in nome in input", () => {
     const nomeVacanza = "nomeVacanza";
     const v = new Vacanza(nomeVacanza);
-    expect(v.nome).to.equal(nomeVacanza + "c");
+    expect(v.nome).to.equal(nomeVacanza);
   });
 });
