4cdbc68e3be8b3da31414007fa16bdc5805cec28
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-08-31 19:24:26 +0200

    Ritorna array vuoto in caso di nessuna transazione

--- tests/DivisioneConto.test.ts
+++ tests/DivisioneConto.test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import DivisioneConto from "../src/DivisioneConto";
 import DivisoriConto from "../src/interfaccie/DivisoriConto";
 import Vacanza from "../src/Vacanza";
+import Persona from "./../src/interfaccie/Persona";
 
 describe("divisioneConto", () => {
   it("Deve ritornare un array vuoto di transazioni quando non esistono persone nella vacanza", () => {
@@ -9,5 +10,16 @@ describe("divisioneConto", () => {
     const divisioneConto = new DivisioneConto(v);
     expect(divisioneConto.getTransazioni("Persona")).to.be.an.instanceOf(Array)
       .to.be.empty;
+  });
+
+  it("Deve ritornare un array vuoto di transazioni quando non esistono spese nella vacanza", () => {
+    const v = new Vacanza("Nome Vacanza");
+    const persona: Persona = { nome: "Perosna1" };
+    v.addPersona(persona);
+    v.addPersona("Persona2");
+    const divisioneConto = new DivisioneConto(v);
+    expect(divisioneConto.getTransazioni(persona.nome)).to.be.an.instanceOf(
+      Array
+    ).to.be.empty;
   });
 });
